5d0ebfaa709068462826f417bd0bb125d7b6f6a0
Farrukh <fuzailof@gmail.com>
2020-10-30 18:30:05 +0500

    Folder structure fixes & New gulp task to clean up dist folder.

--- gulpfile.js
+++ gulpfile.js
@@ -9,43 +9,38 @@ const autoprefixer = require('gulp-autoprefixer');
 const groupMQ = require('gulp-group-css-media-queries');
 const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css');
+const del = require('del');
 
-// Copy font to dist folder.
-function copyFonts() {
-    return src('./src/fonts/**/*.*').pipe(dest('dist/fonts'));
-}
-
-// Copy images to dist folder.
-function copyImages() {
-    return src('./src/assets/img/**/*.*').pipe(dest('dist/img'));
+function copyAssets() {
+    return src('./src/assets/**/*').pipe(dest('dist/assets/'));
 }
 
 // Compile EJS.
 function compileEJS() {
-    return src('src/pages/*.ejs')
-        .pipe(ejs({ title: 'gulp-ejs' }))
+    return src('src/pages/**/*.ejs')
+        .pipe(ejs())
         .pipe(rename({ extname: '.html' }))
         .pipe(dest('./dist'));
 }
 
 // Copy JS files to dist.
-function js() {
-    return src('./src/assets/js/*.js')
+function concatCustomJS() {
+    return src('./src/js/*.js')
         .pipe(concat('main.js'))
-        .pipe(dest('dist/js'));
+        .pipe(dest('dist/js/'));
 }
 
 // Concat JS libs to single file, minify and copy to dist.
-function compileJsLibs() {
-    return src('node_modules/materialize-css/dist/js/materialize.min.js')
+function concatJSLibs() {
+    return src(['node_modules/materialize-css/dist/js/materialize.min.js'])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
         .pipe(dest('dist/js/'));
 }
 
 // Compile CSS libs.
-function compileCssLibs() {
-    return src('node_modules/materialize-css/sass/materialize.scss')
+function compileScssLibs() {
+    return src(['node_modules/materialize-css/sass/materialize.scss'])
         .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(autoprefixer({ cascade: false }))
         .pipe(concat('libs.min.css'))
@@ -54,56 +49,60 @@ function compileCssLibs() {
 }
 
 // Compile SASS.
-function compileSASS() {
-    return src(['./src/assets/styles/styles.sass'])
+function compileCustomSass() {
+    return src(['./src/styles/styles.sass'])
         .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(autoprefixer({ cascade: false }))
         .pipe(groupMQ())
         .pipe(sourcemaps.write('maps'))
         .pipe(dest('./dist/css'))
-        .pipe(browserSync.stream({ once: true }));
+}
+
+// Remove dist.
+function removeDistFolder() {
+    return del('dist');
 }
 
 // Watch Files.
-function watch_files() {
+function watchFiles() {
     browserSync.init({
         server: {
             baseDir: 'dist/',
         },
     });
-    watch('src/assets/styles/**/*.sass', compileSASS);
-    watch('src/assets/js/*.js', js).on('change', browserSync.reload);
+    watch('src/assets/styles/**/*', compileCustomSass).on(
+        'change',
+        browserSync.stream
+    );
+    watch('src/assets/js/**/*', concatCustomJS).on('change', browserSync.reload);
     watch(
-        ['src/pages/*.ejs', 'src/layout/*.ejs', 'src/components/*.ejs'],
+        ['src/**/*.ejs'],
         compileEJS
     ).on('change', browserSync.reload);
-    watch('src/components/*.ejs', compileEJS).on('change', browserSync.reload);
 }
 
 // Default 'gulp' command with start local server and watch files for changes.
 exports.default = series(
-    compileCssLibs,
-    compileJsLibs,
+    removeDistFolder,
+    compileScssLibs,
+    concatJSLibs,
     compileEJS,
-    compileSASS,
-    js,
-    compileJsLibs,
-    copyFonts,
-    copyImages,
-    watch_files,
-    compileEJS
+    compileCustomSass,
+    concatCustomJS,
+    copyAssets,
+    watchFiles
 );
 
 // 'gulp build' will build all assets but not run on a local server.
-exports.build = parallel(
-    compileCssLibs,
-    compileJsLibs,
-    compileEJS,
-    compileSASS,
-    js,
-    compileJsLibs,
-    copyFonts,
-    copyImages,
-    compileEJS
-);
+// exports.build = series(
+//     removeDistFolder,
+//     compileScssLibs,
+//     concatJSLibs,
+//     compileEJS,
+//     compileCustomSass,
+//     concatCustomJS,
+//     copyFonts,
+//     copyImages,
+//     watch_files
+// );
